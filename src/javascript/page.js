2b688559614159ee9e2840c10453a66ca00b41fc
benvds <ben@pomodoro.nl>
2015-02-19 22:11:12 +0100

    update board to use flexbox

--- src/javascript/page.js
+++ src/javascript/page.js
@@ -8,23 +8,22 @@ document.addEventListener('DOMContentLoaded', function documentLoaded() {
             BOARD: $('.board')[0],
             FIELDS: $('.field'),
             STATE: $('.state')[0],
-            RESET_BUTTON: $('.reset')[0],
-            RESULT: $('.result')[0]
+            RESET_BUTTON: $('.reset')[0]
         },
         state = TicTacToe.newState();
 
     UI.FIELDS.forEach(function(field) {
-        field.addEventListener('change', function() {
-            state = claimField(state, field);
-            updateUI(UI, state);
+        field.addEventListener('click', function() {
+            if (field.style.backgroundColor === '') {
+                state = claimField(state, field);
+                updateUI(UI, state);
+            }
         });
     });
 
     UI.RESET_BUTTON.addEventListener('click', function() {
         UI.FIELDS.forEach(function(field) {
-            field.checked = false;
-            field.disabled = false;
-            field.parentElement.bgColor = 'white';
+            field.style.backgroundColor = '';
         });
         state = TicTacToe.newState();
         updateUI(UI, state);
@@ -34,20 +33,16 @@ document.addEventListener('DOMContentLoaded', function documentLoaded() {
 });
 
 function claimField(state, field) {
-    field.disabled = true;
-    field.parentElement.bgColor = TicTacToe.currentPlayer(state);
+    field.style.backgroundColor = TicTacToe.currentPlayer(state);
 
     return TicTacToe.claimPosition(state, inputFieldPosition(field));
 }
 
 // returns input field position in table: [x, y]
 function inputFieldPosition(field) {
-    var parentTableData = field.parentElement,
-        parentTableRow = parentTableData.parentElement;
-
     return {
-        row: dom.elementIndex(parentTableData),
-        column: dom.elementIndex(parentTableRow)
+        row: dom.elementIndex(field.parentElement),
+        column: dom.elementIndex(field)
     };
 }
 
@@ -55,17 +50,9 @@ function updateUI(UI, state) {
     // TODO retrieve stateName (won/draw/playing) and handle with gameStateHandlers
     if (TicTacToe.winner(state)) {
         UI.STATE.innerHTML = 'winner: ' + TicTacToe.winner(state);
-        disableFields(UI.FIELDS);
     } else if (TicTacToe.isDraw(state)) {
         UI.STATE.innerHTML = 'draw';
-        disableFields(UI.FIELDS);
     } else {
         UI.STATE.innerHTML = 'Turn: ' + TicTacToe.currentPlayer(state);
     }
-}
-
-function disableFields(fields) {
-    fields.forEach(function(field) {
-        field.disabled = true;
-    });
 }
