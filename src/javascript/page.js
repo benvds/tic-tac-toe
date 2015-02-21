b1bf8028a60dd80b0b0b6d38235627b137edaac6
benvds <ben@pomodoro.nl>
2015-02-21 12:59:46 +0100

    refactor board init & reset

--- src/javascript/page.js
+++ src/javascript/page.js
@@ -3,56 +3,65 @@ var util = require('./lib/util'),
     $ = require('./lib/dom').$;
     TicTacToe = require('./tictactoe');
 
-
-var UI_UPDATE_HANDLERS = {
-    'PLAYING': function UiUpdatePlaying(UI, state) {
-        UI.STATE.innerHTML = 'Turn: ' + TicTacToe.currentPlayer(state);
-    },
-    'WON': function UiUpdateWon(UI, state) {
-        UI.STATE.innerHTML = 'Winner: ' + TicTacToe.winner(state);
-    },
-    'DRAW': function UiUpdateDraw(UI, state) {
-        UI.STATE.innerHTML = 'Draw';
-    }
-};
-
-// TODO
-//  - extract board initialization
-//  - use that on reset
-//  - remove event handlers when field is claimed
-document.addEventListener('DOMContentLoaded', function documentLoaded() {
-    var UI = {
+function initBoard() {
+    var ui = {
             BOARD: $('.board')[0],
             FIELDS: $('.field'),
             STATE: $('.state')[0],
-            RESET_BUTTON: $('.reset')[0]
+            RESET_BUTTON: $('.reset')[0],
+            fieldEvents: [] // allows for removal of events
         },
         state = TicTacToe.newState();
 
-    UI.FIELDS.forEach(function(field) {
-        field.addEventListener('click', function() {
-            if (field.style.backgroundColor === '') {
-                state = claimField(state, field);
-                updateUI(UI, state);
-            }
-        });
-    });
+    initFields(ui, state);
+    initResetButton(ui);
+    updateUI(ui, state);
+}
 
-    UI.RESET_BUTTON.addEventListener('click', function() {
-        UI.FIELDS.forEach(function(field) {
-            field.style.backgroundColor = '';
-        });
-        state = TicTacToe.newState();
-        updateUI(UI, state);
-    });
+function initFields(ui, state) {
+    ui.FIELDS.forEach(function(field) {
+        function handleFieldClick() {
+            state = claimField(state, field);
+            removeClickEventHandler(ui.fieldEvents, field);
+            updateUI(ui, state);
+        };
 
-    updateUI(UI, state);
-});
+        field.style.backgroundColor = '';
+        field.addEventListener('click', handleFieldClick);
+        ui.fieldEvents.push({ element: field, eventHandler: handleFieldClick });
+    });
+}
+
+function initResetButton(ui) {
+    function handleResetClick() {
+        removeFieldEvents(ui);
+        ui.RESET_BUTTON.removeEventListener('click', handleResetClick);
+        initBoard();
+    }
+
+    ui.RESET_BUTTON.addEventListener('click', handleResetClick);
+}
 
 function claimField(state, field) {
     field.style.backgroundColor = TicTacToe.currentPlayer(state);
 
     return TicTacToe.claimPosition(state, inputFieldPosition(field));
+}
+
+function removeFieldEvents(ui) {
+    ui.FIELDS.forEach(function(field) {
+        removeClickEventHandler(ui.fieldEvents, field);
+    });
+}
+
+function removeClickEventHandler(fieldEvents, field) {
+    var fieldEvent = fieldEvents.filter(function(fieldEvent) {
+        return fieldEvent.element === field;
+    })[0];
+
+    if (fieldEvent) {
+        field.removeEventListener('click', fieldEvent.eventHandler);
+    }
 }
 
 function inputFieldPosition(field) {
@@ -62,7 +71,23 @@ function inputFieldPosition(field) {
     };
 }
 
-function updateUI(UI, state) {
+var UI_UPDATE_HANDLERS = {
+    'PLAYING': function UiUpdatePlaying(ui, state) {
+        ui.STATE.innerHTML = 'Turn: ' + TicTacToe.currentPlayer(state);
+    },
+    'WON': function UiUpdateWon(ui, state) {
+        ui.STATE.innerHTML = 'Winner: ' + TicTacToe.winner(state);
+        removeFieldEvents(ui);
+    },
+    'DRAW': function UiUpdateDraw(ui, state) {
+        ui.STATE.innerHTML = 'Draw';
+        removeFieldEvents(ui);
+    }
+};
+
+function updateUI(ui, state) {
     var stateHandler = UI_UPDATE_HANDLERS[TicTacToe.stateName(state)];
-    stateHandler.call(null, UI, state);
+    stateHandler.call(null, ui, state);
 }
+
+document.addEventListener('DOMContentLoaded', initBoard);
